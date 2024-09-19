55a69f7aca8036d153ae20b681b1593e056891ed
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-18 20:09:24 -0400

    Showing the score

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'A', 'A', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -14,6 +15,7 @@ form.addEventListener('submit', e => {
     }
   });
 
-  console.log(`Score: ${score}`);
+  result.querySelector('span').textContent = `${score}%`;
+  result.classList.remove('d-none');
 
 })
