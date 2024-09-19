04510d132d9483f4d6ccaae786c280017309994b
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-18 20:19:30 -0400

    Animating the score

--- app.js
+++ app.js
@@ -16,7 +16,18 @@ form.addEventListener('submit', e => {
   });
 
   scrollTo(0,0);
-  result.querySelector('span').textContent = `${score}%`;
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 25);
+
+  
   result.classList.remove('d-none');
 
 })
